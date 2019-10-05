95ead2572332bec6ba9e2bde8a7ab8926c870ac1
Giksai <vermione9@gmail.com>
2019-10-05 14:13:24 +0300

    Fixed code

---
+++ README.md
@@ -0,0 +1,3 @@
+## Second task
+Command example: node program.js /Users/evgenij/Projects/automation_mikhailouski_5_2 .txt
+Wich searches for the last created file with .txt extention in given directory.
--- program.js
+++ program.js
@@ -1,72 +1,84 @@
-var http = require('http');
-var path = require('path');
-var fs = require ('fs');
-var url = require('url');
+const http = require('http'),
+    server = http.createServer(),
+    pathM = require('path');
+    fs = require ('fs'),
+    url = require('url');
 
-http.createServer((req, res) =>
-{
-    var inputParams = url.parse(req.url, true).query;
-    fs.readdir(inputParams.path, (error, files) =>
-    {
-        if(error) return console.log(err);
+const args = process.argv;
 
-        var compareTime = Date.parse('1/1/1970 00:00');
-        var lastCreatedFileHolder = files[0];
-        var filesWithCorrectExtention = new Array();
+//Starting point
+server.listen(8080, () => {
+    if(args.length !== 4) {
+        console.log('Wrong commands!');
+        console.log('Use: node program.js <path> <extention>');
+        console.log('Example: node program.js /Users/evgenij/Projects/ .txt');
+        process.exit();
+    }
+
+    execute(args[2], args[3]);
+});
+
+function execute(path, ext) {
+    //Getting all files in given directory
+    fs.readdir(path, (error, files) => {
+        if(error) {
+            console.log(error);
+            process.exit();
+        }
+
+        //Time variable that is used to find a file with the last created time
+        let compareTime = Date.parse('1/1/1970 00:00');
+        //Holds last created file
+        let lastCreatedFileHolder = files[0];
+        let filesWithCorrectExtention = [];
 
         //Filtering files with the wrong extention
-        //And searching for the last created file
-        files.forEach((file)=>
-        {
-            if(getFileExt(file) != inputParams.ext)
+        //and searching for the last created file
+        files.forEach((file) => {
+            if(pathM.extname(file) !== ext) {
                 return;
+            }
                 
             filesWithCorrectExtention.push(file);
 
-            let filePath = inputParams.path + '/' + file;
+            let filePath = pathM.join(path, file);
             let createdTime = fs.statSync(filePath).ctime;
             
-            if(createdTime > compareTime)
-            {
+            if(createdTime > compareTime) {
                 compareTime = createdTime;
                 lastCreatedFileHolder = file;
             }
         });
 
-        //finding files with created time within 10
-        //seconds from a last created file
+        //Getting creation time of the last created file
         var lastCreatedFileTime = fs.statSync(lastCreatedFileHolder).ctime;
         var closeCreatedFiles = [];
-        filesWithCorrectExtention.forEach((file)=>
-        {
+
+        //Finding files with created time within 10
+        //seconds from a last created file
+        filesWithCorrectExtention.forEach((file) => {
             let createdTime = fs.statSync(file).ctime;
             if(lastCreatedFileTime.getTime() - createdTime.getTime() < 10000
-            && file != lastCreatedFileHolder)
+            && file !== lastCreatedFileHolder) {
                 closeCreatedFiles.push(file);
-
+            }
         });
 
         //Printing found files
-        if(getFileExt(lastCreatedFileHolder) != inputParams.ext)
+        if(pathM.extname(lastCreatedFileHolder) !== ext) {
             console.log("File not found");
-        else
+        } else {
             console.log("Last created file: " + lastCreatedFileHolder);
+        }
         
-        if(closeCreatedFiles.length > 0)
-        {
+        //Printing files created within 10 seconds of last created file
+        if(closeCreatedFiles.length > 0) {
             console.log("Files created within 10 seconds:");
-            closeCreatedFiles.forEach((file)=>
-            {
+            closeCreatedFiles.forEach((file) => {
                 console.log(file);
             });
         }
-        
+        process.exit();
     });
-
-    res.end();
-}).listen(8080);
-
-function getFileExt(file)
-{
-    return file.split('.')[file.split('.').length-1];
+    
 }
